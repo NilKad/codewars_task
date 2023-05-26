cca5272f92b0ae5b88da9458b5a68a6db9681fa8
NilKad <kadulin@gmail.com>
2023-05-26 08:09:36 +0300

    20

--- task20-most_frequently_used_words_in_a_text.js
+++ task20-most_frequently_used_words_in_a_text.js
@@ -1,32 +1,44 @@
 function topThreeWords(text) {
   const countWords = new Map();
   const words = text.trim().split(/[^a-zA-Z']+/);
-  // console.log(words);
   words.forEach((el) => {
     const newEl = el.toLowerCase();
-    if (countWords.has(newEl)) {
-      countWords.set(newEl, countWords.get(newEl) + 1);
-    } else {
-      el !== "" && countWords.set(newEl, 1);
-    }
+    countWords.set(newEl, countWords.has(newEl) ? countWords.get(newEl) + 1 : 1);
+    // if (countWords.has(newEl)) {
+    //   countWords.set(newEl, countWords.get(newEl) + 1);
+    // } else {
+    //   el !== "" && el !== "'" && countWords.set(newEl, 1);
+    // }
   });
-  console.log(countWords);
+  countWords.delete("'");
+  countWords.delete("");
+
+  // console.log(
+  //   "countWords: ",
+  //   [...countWords]
+  //     .sort((a, b) => b[1] - a[1])
+  //     .slice(0, 3)
+  //     .map((a) => a[0])
+  // );
   const values = Array.from(countWords.values()).sort((a, b) => b - a);
-  // const values = Object.values(countWords);
-  // values = values;
   const res = [];
-  console.log(values);
-  for (let i = 0; i < 3 && i < values.length; i++) {
-    countWords.forEach((value, key) => {
-      // console.log("key: ", key, "\tvalue: ", value);
-      if (value === values[i]) {
-        res.push(key);
-      }
-    });
-  }
-  console.log("res: ", res);
+  // for (let i = 0; i < 3 && i < values.length; i++) {
+  //   for (const item of countWords) {
+  //     if (item[1] === values[i] && !res.includes(item[0])) {
+  //       res.push(item[0]);
+  //       break;
+  //     }
+  //   }
+  // }
+  // return res;
+  return [...countWords]
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3)
+    .map((a) => a[0]);
 }
 
+console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e f f f f"), ["e", "d", "a"]);
+console.log(topThreeWords("a b c d e f g h "), ["e", "d", "a"]);
 console.log(topThreeWords("a a a  b  c c  d d d d  e e e e e"), ["e", "d", "a"]);
 
 console.log(topThreeWords("a a c b b"), ["a", "b", "c"]);
@@ -39,18 +51,18 @@ console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e
 
 console.log(topThreeWords("  ///wont won't won't "), ["won't", "wont"]);
 
-// console.log(topThreeWords("  , e   .. "), ["e"]);
+console.log(topThreeWords("  , e   .. "), ["e"]);
 
-// console.log(topThreeWords("  ...  "), []);
+console.log(topThreeWords("  ...  "), []);
 
-// console.log(topThreeWords("  '  "), []);
+console.log(topThreeWords("  '  "), []);
 
-// console.log(
-//   topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
-// mind, there lived not long since one of those gentlemen that keep a lance
-// in the lance-rack, an old buckler, a lean hack, and a greyhound for
-// coursing. An olla of rather more beef than mutton, a salad on most
-// nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-// on Sundays, made away with three-quarters of his income.`),
-//   ["a", "of", "on"]
-// );
+console.log(
+  topThreeWords(`In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.`),
+  ["a", "of", "on"]
+);
